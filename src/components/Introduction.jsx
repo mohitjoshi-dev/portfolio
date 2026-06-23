6b987cd747187b16e026a8af3ae078736850663c
mohitjoshi-dev <mohitjoshi1357@gmail.com>
2026-06-24 01:24:08 +0530

    Initial portfolio setup

---
+++ src/components/Introduction.jsx
@@ -0,0 +1,17 @@
+function Introduction() {
+  return (
+    <section>
+      <h1 className="text-6xl italic text-center text-blue-500">
+         Mohit Joshi
+      </h1>
+      <h2>B.Tech Student & Frontend Developer</h2>
+
+      <p>
+        Building React applications, learning modern web development,
+        Learning and preparing for software internships.
+      </p>
+    </section>
+  );
+}
+
+export default Introduction;
---
+++ src/components/skills.jsx
@@ -0,0 +1,21 @@
+function Skills() {
+  return (
+    <section className="max-w-5xl mx-auto px-6 py-10">
+      <h2 className="text-3xl font-bold mb-6">
+        Skills
+      </h2>
+
+      <div className="flex gap-4 flex-wrap">
+        <div className="bg-gray-800 px-4 py-2 rounded-lg">HTML</div>
+        <div className="bg-gray-800 px-4 py-2 rounded-lg">CSS</div>
+        <div className="bg-gray-800 px-4 py-2 rounded-lg">JavaScript</div>
+        <div className="bg-gray-800 px-4 py-2 rounded-lg">React</div>
+        <div className="bg-gray-800 px-4 py-2 rounded-lg">Tailwind CSS</div>
+        <div className="bg-gray-800 px-4 py-2 rounded-lg">Git</div>
+        <div className="bg-gray-800 px-4 py-2 rounded-lg">GitHub</div>
+      </div>
+    </section>
+  );
+}
+
+export default Skills;
