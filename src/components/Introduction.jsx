794e558d05aa2c8e12ff3eb9e1f7f038014bb82c
mohitjoshi-dev <mohitjoshi1357@gmail.com>
2026-06-25 21:01:32 +0530

    Added project section and updated portfolio

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,15 +1,38 @@
 function Introduction() {
   return (
     <>
-    <section className="min-h-screen bg-gray-950 flex flex-col justify-center items-center text-white">
+    
+    
+
+    <section className="relative min-h-screen bg-gray-950 flex flex-col justify-center items-center text-white">
+
+    <div className="absolute w-80 h-80 bg-purple-700 rounded-full blur-[150px] opacity-20 -top-20 -left-20"></div>
+
+    <div className="absolute w-80 h-80 bg-purple-700 rounded-full blur-[150px] opacity-20 bottom-0 right-0"></div>
+
+    <div className="relative z-10"></div>
+    
+    <div className="inline-flex items-center gap-2 rounded-full border border-purple-500/20 bg-purple-500/10 px-4 py-1.5 text-sm font-medium text-blue-400 backdrop-blur-sm mb-6">
+      <span className="flex h-2 w-2 rounded-full bg-blue-400 animate-pulse"></span>
+      Available for Internships
+    </div>
+
+
+    <p className="text-purple-400 text-3xl font-medium mb-4">
+  👋 Hello, I'm
+    </p>
+
+
+    <h1 className="text-7xl font-extrabold mb-6 tracking-wide drop-shadow-lg"> 
   
-    <h1 className="text-7xl font-bold mb-6"> 
     Mohit Joshi
     </h1>
 
     <h2 className="text-2xl  text-purple-500 font-semibold mb-6">
-    A Simple Frontend Devoloper | Tech Enthusiast | Eager Learner
-    </h2>
+      Frontend Developer
+      Crafting fast, responsive web experiences
+      with React & Tailwind CSS.
+        </h2>
 
     <p className="text-xl text-gray-400 text-center max-w-2x1 mb-8">
         Building modern web applications with React, Tailwind CSS and
@@ -17,34 +40,39 @@ function Introduction() {
     </p>
 
   <div className="flex gap-6">
-  <button className="bg-purple-600 px-6 py-3 rounded-lg">
+  <button className="bg-purple-600 px-6 py-3 rounded-lg cursor-pointer hover:scale-110 hover:shadow-purple-500 hover:shadow-2xl transition-all duration-500">
     View Projects
   </button>
 
-  <button className="border border-purple-500 px-6 py-3 rounded-lg">
+  <button className="border border-purple-500 px-6 py-3 rounded-lg cursor-pointer hover:scale-110 hover:shadow-purple-500 hover:shadow-2xl transition-all duration-500">
     Download Resume
   </button>
 </div>
 
 
-    <nav className="absolute top-0 w-full flex justify-between p-6">
-      <h1 className="text-xl font-bold">Mohit</h1>
+    <nav className="fixed top-1 left-1/2 -translate-x-1/2 z-50 max-w-3x1 flex items-center justify-between px-8 py-3 rounded-full
+                 bg-gray-800/70  backdrop-blur-xl border  border-purple-500/20  shadow-2xl" >
+
+      
 
       <div className="flex gap-6">
-      <a href="#about" className="hover:text-purple-500">
+      <a href="#about" className=" px-4 py-2 rounded-full hover:bg-purple-500/10 hover:text-purple-400 hover:-translate-y-0.5 transition-all duration-500">
         About
       </a>        
-      <a href="#skills" className="hover:text-purple-500">
+      <a href="#skills" className="px-4 py-2 rounded-full hover:bg-purple-500/10 hover:text-purple-400 hover:-translate-y-0.5 transition-all duration-500">
         Skills
       </a>  
-      <a href="#projects" className="hover:text-purple-500">
+      <a href="#projects" className="px-4 py-2 rounded-full hover:bg-purple-500/10 hover:text-purple-400 hover:-translate-y-0.5 transition-all duration-500">
         Projects
       </a>        
-      <a href="#contact" className="hover:text-purple-500">
+      <a href="#contact" className="px-4 py-2 rounded-full hover:bg-purple-500/10 hover:text-purple-400 hover:-translate-y-0.5 transition-all duration-500">
         Contact
-      </a>        
-            
-      </div>
+      </a>               
+    </div>
+      <a href="/resume.pdf" target="_blank" rel="noopener noreferrer"
+      className="px-3 py-4 rounded-full bg-linear-to-r from-purple-60 to-fuchsia-500 font-medium text-white hover:scale-105
+      hover:shadow-1g hover:shadow-violet-500/30 transition-all duration-300 ease-out ml-4"> Resume ↗ </a>
+
   </nav>
 </section>
 
@@ -53,14 +81,15 @@ function Introduction() {
       id="about"
       className="min-h-screen bg-gray-900 text-white flex flex-col justify-center items-center"
     >
-      <h2 className="text-5xl font-bold mb-6">
+      <h2 className="text-5xl font-extrabold tracking-tight mb-6 ">
         About Me
       </h2>
 
-      <p className="text-lg text-gray-300 max-w-3xl text-center">
-        I am a B.Tech student passionate about frontend development,
+      <p className="text-lg text-gray-500 max-w-3xl text-center">
+        I am a B.Tech student who is currently in 3rd year pursuing Computer Science and I am passionate about frontend development,
         React, Tailwind CSS and building projects that solve real-world
         problems.
+        I till now have not made that much of a Project but will make sure to make them sooner
         
       </p>
   </section>
--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -13,12 +13,12 @@ function Skills() {
            Skills
         </h2>
 
-      <div className="w-32 h-1 bg-gradient-to-br from-gray-900 to-purple-950 mx-auto mt-3 rounded-full"></div>
+      <div className="w-32 h-1 bg-linear-to-br from-gray-900 to-purple-950 mx-auto mt-3 rounded-full"></div>
     </div>
 
       <div className="grid grid-cols-3 gap-6">
 
-        <div className="bg-gradient-to-br from-gray-900 to-purple-950 w-46 h-30 rounded-xl flex flex-col items-center cursor-pointer justify-center hover:scale-110 hover:shadow-purple-500 hover:shadow-2xl transition-all duration-500">
+        <div className="bg-linear-to-br from-gray-900 to-purple-950 w-46 h-30 rounded-xl flex flex-col items-center cursor-pointer justify-center hover:scale-110 hover:shadow-purple-500 hover:shadow-2xl transition-all duration-500">
 
         <FaHtml5 className="text-5xl text-orange-500 mb-3" />
 
@@ -28,7 +28,7 @@ function Skills() {
 
         </div>  
 
-        <div className="bg-gradient-to-br from-gray-900 to-purple-950 w-46 h-30 rounded-xl flex flex-col items-center justify-center hover:scale-110 hover: shadow-purple-500 hover:shadow-2xl cursor-pointer transition-all duration-500">
+        <div className="bg-linear-to-br from-gray-900 to-purple-950 w-46 h-30 rounded-xl flex flex-col items-center justify-center hover:scale-110 hover: shadow-purple-500 hover:shadow-2xl cursor-pointer transition-all duration-500">
 
         <FaCss3Alt className="text-5xl text-blue-500 mb-3" />
             <h3 className="text-xl font-semibold">
@@ -36,7 +36,7 @@ function Skills() {
             </h3>
 
         </div>  
-        <div className="bg-gradient-to-br from-gray-900 to-purple-950 w-46 h-30 rounded-xl flex flex-col items-center justify-center hover:scale-110 hover:shadow-purple-500 hover:shadow-2xl cursor-pointer transition-all duration-500">
+        <div className="bg-linear-to-br from-gray-900 to-purple-950 w-46 h-30 rounded-xl flex flex-col items-center justify-center hover:scale-110 hover:shadow-purple-500 hover:shadow-2xl cursor-pointer transition-all duration-500">
 
         <FaJava className="text-5xl text-red-500 mb-3" />  
             <h3 className="text-xl font-semibold">
@@ -44,7 +44,7 @@ function Skills() {
             </h3>
 
         </div>  
-        <div className="bg-gradient-to-br from-gray-900 to-purple-950 w-46 h-30 rounded-xl flex flex-col items-center justify-center hover:scale-110 hover:shadow-purple-500 hover:shadow-2xl cursor-pointer transition-all duration-500">
+        <div className="bg-linear-to-br from-gray-900 to-purple-950 w-46 h-30 rounded-xl flex flex-col items-center justify-center hover:scale-110 hover:shadow-purple-500 hover:shadow-2xl cursor-pointer transition-all duration-500">
 
         <SiJavascript className="text-5xl text-yellow-400 mb-3" />   
             <h3 className="text-xl font-semibold">
@@ -52,14 +52,14 @@ function Skills() {
             </h3>
 
         </div>  
-        <div className="bg-gradient-to-br from-gray-900 to-purple-950 w-46 h-30 rounded-xl flex flex-col items-center justify-center hover:scale-110 hover:shadow-purple-500 hover:shadow-2xl cursor-pointer transition-all duration-500">
+        <div className="bg-linear-to-br from-gray-900 to-purple-950 w-46 h-30 rounded-xl flex flex-col items-center justify-center hover:scale-110 hover:shadow-purple-500 hover:shadow-2xl cursor-pointer transition-all duration-500">
         <FaPython className="text-5xl text-yellow-500 mb-3" />  
             <h3 className="text-xl font-semibold">
                 PYTHON
             </h3>
 
         </div>  
-        <div className="bg-gradient-to-br from-gray-900 to-purple-950 w-46 h-30 rounded-xl flex flex-col items-center justify-center hover:scale-110 hover:shadow-purple-500 hover:shadow-2xl cursor-pointer transition-all duration-500">
+        <div className="bg-linear-to-br from-gray-900 to-purple-950 w-46 h-30 rounded-xl flex flex-col items-center justify-center hover:scale-110 hover:shadow-purple-500 hover:shadow-2xl cursor-pointer transition-all duration-500">
 
         <FaReact className="text-5xl text-cyan-400 mb-3" />   
             <h3 className="text-xl font-semibold">
@@ -67,7 +67,7 @@ function Skills() {
             </h3>
 
         </div>  
-        <div className="bg-gradient-to-br from-gray-900 to-purple-950 w-46 h-30 rounded-xl flex flex-col items-center justify-center hover:scale-110 hover:shadow-purple-500 hover:shadow-2xl cursor-pointer transition-all duration-500">
+        <div className="bg-linear-to-br from-gray-900 to-purple-950 w-46 h-30 rounded-xl flex flex-col items-center justify-center hover:scale-110 hover:shadow-purple-500 hover:shadow-2xl cursor-pointer transition-all duration-500">
 
         <FaCode className="text-5xl text-blue-400 mb-3" />  
             <h3 className="text-xl font-semibold">
@@ -75,7 +75,7 @@ function Skills() {
             </h3>
 
         </div>  
-        <div className="bg-gradient-to-br from-gray-900 to-purple-950 w-46 h-30 rounded-xl flex flex-col items-center justify-center hover:scale-110 hover:shadow-purple-500 hover:shadow-2xl cursor-pointer transition-all duration-500">
+        <div className="bg-linear-to-br from-gray-900 to-purple-950 w-46 h-30 rounded-xl flex flex-col items-center justify-center hover:scale-110 hover:shadow-purple-500 hover:shadow-2xl cursor-pointer transition-all duration-500">
 
         <SiMysql className="text-5xl text-blue-500 mb-3" />
             <h3 className="text-xl font-semibold">
@@ -83,7 +83,7 @@ function Skills() {
             </h3>
 
         </div>  
-        <div className="bg-gradient-to-br from-gray-900 to-purple-950 w-46 h-30 rounded-xl flex flex-col items-center justify-center hover:scale-110 hover:shadow-purple-500 hover:shadow-2xl cursor-pointer transition-all duration-500">
+        <div className="bg-linear-to-br from-gray-900 to-purple-950 w-46 h-30 rounded-xl flex flex-col items-center justify-center hover:scale-110 hover:shadow-purple-500 hover:shadow-2xl cursor-pointer transition-all duration-500">
 
          <SiTailwindcss className="text-5xl text-cyan-400 mb-3" />
             <h3 className="text-xl font-semibold">
@@ -91,7 +91,7 @@ function Skills() {
             </h3>
 
         </div>  
-        <div className="bg-gradient-to-br from-gray-900 to-purple-950 w-46 h-30 rounded-xl flex flex-col items-center className= col-start-2 justify-center hover:scale-110 hover:shadow-purple-500 hover:shadow-2xl cursor-pointer transition-all duration-500">
+        <div className="bg-linear-to-br from-gray-900 to-purple-950 w-46 h-30 rounded-xl flex flex-col items-center className= col-start-2 justify-center hover:scale-110 hover:shadow-purple-500 hover:shadow-2xl cursor-pointer transition-all duration-500">
               
          <FaGithub className="text-5xl text-white mb-3" />
             <h3 className="text-xl font-semibold">
